7054d32573bdf8ce93d9e9ee078b05614e823fcc
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-05-17 19:14:55 -0400

    use https redirection in production

--- Program.cs
+++ Program.cs
@@ -2,18 +2,14 @@ using api.Models;
 using api.Services;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Load .env file (optional, for local dev)
 DotNetEnv.Env.Load(); // requires DotNetEnv NuGet package
 
-// Replace connection string manually from env
-var connectionString = Environment.GetEnvironmentVariable("PG_MESSAGES_CONNECTION_STRING");
-
-if (connectionString == null)
-{
-    throw new Exception("No environment variable for connection string found!");
-}
+// Set connection using env. Crash if we don't have one.
+string connectionString = Environment.GetEnvironmentVariable("PG_MESSAGES_CONNECTION_STRING") 
+    ?? throw new InvalidOperationException("Missing environment variable: PG_MESSAGES_CONNECTION_STRING");
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -22,6 +18,8 @@ builder.Services.AddDbContext<SalesDbContext>(options =>
 
 // Register the repository services (ensure this matches your actual repository class)
 builder.Services.AddScoped<IRepository<Message, Guid>, MessageRepository>();
+
+// CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -29,14 +27,16 @@ builder.Services.AddCors(options =>
         policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
     });
 });
-var app = builder.Build();
+
+WebApplication app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-}
+    app.MapOpenApi(); // Show errors to end user when in development mode.
 
-app.UseHttpsRedirection();
+}
+app.UseHttpsRedirection(); // make sure you have ssl set up.
 app.UseCors("AllowAll");
 app.MapControllers();
 
